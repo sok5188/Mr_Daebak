0cba61a3f6e72720031273751a43877c6cafafcb
sok5188 <sok5188@gmail.com>
2022-11-15 22:40:40 +0900

    update details

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var session = require("express-session");
+var path = require("path");
 var fs = require("fs");
 //var FileStore = require("session-file-store")(session);
 var app = express();
@@ -11,13 +12,14 @@ var compression = require("compression");
 var helmet = require("helmet");
 var homeRouter = require("./routes/home.js");
 var authRouter = require("./routes/auth.js");
-var orderRouter = require("./routes/order.js");
+var menuRouter = require("./routes/menu.js");
 var manageRouter = require("./routes/manage.js");
-
+var cors = require("cors");
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-
+app.use(cors());
+app.use(express.json());
 app.use(
   session({
     secret: "keyboard cat",
@@ -26,12 +28,15 @@ app.use(
     store: new FileStore(),
   })
 );
+app.use(express.static(path.join(__dirname, "/PizzaWebsite/build")));
 
-app.use("/", homeRouter);
+//app.use("/", homeRouter);
 app.use("/auth", authRouter);
-app.use("/order", orderRouter);
+app.use("/menu", menuRouter);
 app.use("/manage", manageRouter);
-
+app.get("*", function (request, response) {
+  response.sendFile(path.join(__dirname, "/PizzaWebsite/build/index.html"));
+});
 app.use(function (req, res, next) {
   res.status(404).send("Sorry cant find that!");
 });
