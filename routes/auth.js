84debc657d378668fbf33ac8bb38a23ae92118aa
sok5188 <sok5188@gmail.com>
2022-11-17 20:00:12 +0900

    edited routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,8 @@
 var express = require("express");
 var router = express.Router();
-var path = require("path");
 var db = require("../lib/db.js");
-const cors = require("cors");
 const util = require("util");
-const session = require("express-session");
-/*
-router.get("/login", function (req, res) {
-  console.log("entered login page");
-  let tmp_html = `
-        <form action="/auth/login_process" method="post">
-            <p><input type="text" name="id" placeholder="id"></p>
-            <p><input type="password" name="password" placeholder="password"></p>
-            <p>
-                <input type="submit" value="login">
-            </p>
-        </form>
-    `;
-  res.send(tmp_html);
-});
-*/
+
 router.post("/login_process", function (request, response) {
   //implement Login function
   console.log("entered login process");
@@ -69,23 +52,6 @@ router.get("/check_login", function (request, response) {
     response.json({ name: "", islogin: "False" });
   }
 });
-
-// router.get("/signin", function (request, response) {
-//   let tmp_html = `
-//         <form action="/auth/signin_process" method="post">
-//             <p><input type="text" name="id" placeholder="id"></p>
-//             <p><input type="password" name="password" placeholder="password"></p>
-//             <p><input type="text" name="name" placeholder="name"></p>
-//             <p><input type="number" name="credit" placeholder="credit"></p>
-//             <p><input type="number" name="phone" placeholder="phone"></p>
-//             <p>
-//                 <input type="submit" value="signin">
-//             </p>
-//         </form>
-//     `;
-//   response.send(tmp_html);
-// });
-
 router.post("/signin_process", function (request, response) {
   var post = request.body;
   var id = post.id;
@@ -93,28 +59,29 @@ router.post("/signin_process", function (request, response) {
   var name = post.name;
   var credit = post.credit;
   var phone = post.phone;
+  var address = post.address;
   var visit = 0;
   //id중복체크 (나중에 아이디 옆에 중복확인 버튼을 만들어서 중복확인프로세스를 따로 만들어서 처리하고 여기서는 바로 insert)
   db.query(`SELECT id FROM AUTH WHERE id=?`, [id], function (id) {
     console.log(id);
     if (id != null) {
       //이미 auth에 해당 id 존재
-      //일단은 그냥 리다이렉트
-      response.redirect("/signin");
+      response.send("ID Fail");
     }
     console.log("not in table");
   });
   db.query(
-    `INSERT INTO AUTH VALUES(?,?,?,?,?,?)`,
-    [id, name, credit, phone, visit, password],
+    `INSERT INTO AUTH VALUES(?,?,?,?,?,?,?)`,
+    [id, name, credit, phone, visit, password, address],
     function (err, result) {
       if (err) {
-        console.log("error!!");
+        response.send("INSERT Fail");
         throw err;
       }
       //3xx는 redirection을 의미 (1xx는 info, 2xx:success, 4xx:client err, 5xx:server err)
-      response.writeHead(302, { Location: `/` });
-      response.end();
+      else {
+        response.send("Success");
+      }
     }
   );
 });
